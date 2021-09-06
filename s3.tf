d98a19eedbf8f9d2f447b122985fb4c4d3850a4b
Masato Yagi <yaginen9@gmail.com>
2021-09-07 01:55:06 +0900

    style: 余分なスポースを削除

--- s3.tf
+++ s3.tf
@@ -14,8 +14,8 @@ resource "aws_s3_bucket_public_access_block" "recruit-bucket-access-bucket" {
   bucket = aws_s3_bucket.recruit-bucket-2.id
 
   # Block public access (bucket settings)
-  block_public_acls   = true
-  ignore_public_acls  = true
-  block_public_policy = true
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
   restrict_public_buckets = true
 }
