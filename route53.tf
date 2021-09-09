f9a92e8254d4df274ec690f3072fc7bd9b876454
Masato Yagi <yaginen9@gmail.com>
2021-09-10 01:05:03 +0900

    feat: Route53のAレコードとホストゾーンの登録（未動作確認）

---
+++ route53.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_zone" "recruit-rits" {
+  name = "recruit-rits.net"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "aws_route53_zone.recruit-rits.zone_id"
+  name    = "www.recruit-rits.net"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_eip.recruit_ip]
+}
