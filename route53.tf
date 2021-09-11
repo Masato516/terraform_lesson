dd9d8e793b3806e0bd761f278eba8df23cc16f60
Masato Yagi <yaginen9@gmail.com>
2021-09-11 16:57:05 +0900

    fix: valueの指定ミスの修正

--- route53.tf
+++ route53.tf
@@ -3,9 +3,9 @@ resource "aws_route53_zone" "recruit-rits" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = "aws_route53_zone.recruit-rits.zone_id"
+  zone_id = aws_route53_zone.recruit-rits.zone_id
   name    = "www.recruit-rits.net"
   type    = "A"
   ttl     = "300"
-  records = [aws_eip.recruit_ip]
+  records = [aws_eip.recruit_ip.public_ip]
 }
