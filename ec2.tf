0f621b916ba8d7371cabf3e860fbc8f4617ee295
Masato Yagi <yaginen9@gmail.com>
2021-09-12 22:42:20 +0900

    refactor: コードのフォーマッティング

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "recruit_web_server" {
-  ami           = "ami-09ebacdc178ae23b7" # Amazon Linux 2
-  instance_type = "t2.micro"
+  ami                         = "ami-09ebacdc178ae23b7" # Amazon Linux 2
+  instance_type               = "t2.micro"
   associate_public_ip_address = "true" # IPアドレスを割り当てる
-  vpc_security_group_ids = [aws_security_group.sg_for_public_subnet.id]
-  subnet_id     = aws_subnet.recruit_web_1c.id
+  vpc_security_group_ids      = [aws_security_group.sg_for_public_subnet.id]
+  subnet_id                   = aws_subnet.recruit_web_1c.id
 
   tags = {
     Name = "recruit_web_server"
--- security_group.tf
+++ security_group.tf
@@ -1,3 +1,27 @@
+resource "aws_security_group" "sg_for_public_subnet" {
+  name        = "sg_for_public_subnet"
+  description = "This sg is for recruit web"
+  vpc_id      = aws_vpc.recruit_web.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_for_recruit_web"
+  }
+}
+
 resource "aws_security_group" "sg_for_db_subnet" {
   name        = "sg_for_db_subnet"
   description = "This sg is for db of recruit web"
