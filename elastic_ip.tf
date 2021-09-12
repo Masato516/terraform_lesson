cdf8070aa7b74cb48cc1d28e4fbc074ba5cd4ed8
Masato Yagi <yaginen9@gmail.com>
2021-09-12 19:24:47 +0900

    feat: public IPアドレスとDNSを出力 close #4

--- elastic_ip.tf
+++ elastic_ip.tf
@@ -7,4 +7,13 @@ resource "aws_eip" "recruit_ip" {
   }
 }
 
-# TODO: 割り当てられたIPアドレスを出力する
+# 割り当てられたIPアドレスを出力
+output "public_ip" {
+  value       = aws_instance.recruit_web_server.public_ip
+  description = "The public IP address of the main server instance."
+}
+
+output "public_dns" {
+  value       = aws_instance.recruit_web_server.public_dns
+  description = "The public dns of the main server instance."
+}
