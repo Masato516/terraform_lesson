bf94927352ee8584347962e021920fccd99ccaee
Masato Yagi <yaginen9@gmail.com>
2021-09-25 02:03:27 +0900

    feat: S3を操作するIAMユーザーの作成・アタッチ #20

--- s3.tf
+++ s3.tf
@@ -19,3 +19,40 @@ resource "aws_s3_bucket_public_access_block" "recruit_access_bucket" {
   block_public_policy     = true
   restrict_public_buckets = true
 }
+
+# S3を操作するためのIAMユーザー
+resource "aws_iam_user" "s3_user" {
+  name = "s3-user"
+}
+
+# S3のfull access権限を持つポリシーをIAMユーザーにアタッチ
+resource "aws_iam_user_policy" "s3" {
+  name   = "s3_policy"
+  user   = aws_iam_user.s3_user.name
+  policy = data.aws_iam_policy_document.s3_full_access_policy.json
+}
+
+# S3のIAM access key を作成
+resource "aws_iam_access_key" "s3" {
+  user = aws_iam_user.s3_user.name
+}
+
+# S3のfull access権限のポリシーデータを取得
+data "aws_iam_policy_document" "s3_full_access_policy" {
+  statement {
+    actions   = ["s3:*"]
+    resources = ["*"]
+    effect = "Allow"
+  }
+}
+
+# IAM access key を出力
+output "aws_iam_s3_access_key" {
+  value     = aws_iam_access_key.s3.id
+  sensitive = true
+}
+# IAM secret access key を出力
+output "aws_s3_iam_secret" {
+  value     = aws_iam_access_key.s3.secret
+  sensitive = true
+}
